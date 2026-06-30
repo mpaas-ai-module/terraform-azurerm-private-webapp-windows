56c8b0b26d70b36cb7a844f42b56e2365b2d06fa
gaurav-d9 <gaurav@bootlabstech.com>
2026-06-30 17:31:34 +0530

    Import from bootlabstech + DAG producer outputs (mpaas-ai-module migration)

---
+++ main.tf
@@ -0,0 +1,112 @@
+# Creates a Windows webapp
+resource "azurerm_windows_web_app" "example" {
+  name                      = var.name
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  service_plan_id           = var.service_plan_id
+  https_only                = var.https_only
+  virtual_network_subnet_id = var.virtual_network_subnet_id
+
+  site_config {
+    ftps_state             = var.ftps_state
+    vnet_route_all_enabled = var.vnet_route_all_enabled
+    app_command_line       = var.app_command_line
+    use_32_bit_worker      = var.use_32_bit_worker
+
+    dynamic "ip_restriction" {
+      for_each = var.ip_restrictions
+
+      content {
+        action     = "Allow"
+        headers    = []
+        ip_address = ip_restriction.value.ip_address
+        name       = ip_restriction.key
+        priority   = ip_restriction.value.priority
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "docker" ? [1] : []
+      content {
+        docker_image_name        = var.stack_version
+        docker_registry_url      = var.docker_registry_url
+        docker_registry_username = var.docker_registry_username
+        docker_registry_password = var.docker_registry_password
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "dotnet" ? [1] : []
+      content {
+        dotnet_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "dotnetcore" ? [1] : []
+      content {
+        dotnet_core_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "java" ? [1] : []
+      content {
+        java_version = var.stack_version
+
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "node" ? [1] : []
+      content {
+        node_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "php" ? [1] : []
+      content {
+        php_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "python" ? [1] : []
+      content {
+        python = var.python
+      }
+    }
+    virtual_application {
+      preload       = true
+      physical_path = var.physical_path
+      virtual_path  = var.virtual_path
+    }
+    virtual_application {
+      preload       = true
+      physical_path = var.physical_path1
+      virtual_path  = var.virtual_path1
+    }
+
+  }
+}
+
+# Creates  a private endpoint with private dns
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = azurerm_windows_web_app.example.id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,196 @@
+variable "name" {
+  type        = string
+  description = "The name which should be used for this Windows Windows Web App."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Resource Group where the Windows Web App should exist."
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure Region where the Windows Web App should exist.Central India or South India"
+}
+
+variable "service_plan_id" {
+  type        = string
+  description = "The ID of the Service Plan that this Windows App Service will be created in."
+
+}
+
+variable "ftps_state" {
+  type        = string
+  description = "The State of FTP / FTPS service. Possible values include: AllAllowed, FtpsOnly, Disabled."
+  default     = "FtpsOnly"
+}
+
+variable "app_command_line" {
+  type        = string
+  description = "The App command line to launch."
+  default     = ""
+}
+variable "current_stack" {
+  type        = string
+  description = " The Application Stack for the Windows Web App. Possible values include dotnet, dotnetcore, node, python, php, and java."
+}
+
+variable "stack_version" {
+  type        = string
+  description = "Version of the selected stack."
+
+}
+
+# variable "docker_registry_url" {
+#   type        = string
+#   description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
+#   default     = "https://mcr.microsoft.com"
+# }
+# variable "docker_registry_username" {
+#   type        = string
+#   description = "The User Name to use for authentication against the registry to pull the image."
+#   default     = "admin"
+
+# }
+# variable "docker_registry_password" {
+#   type        = string
+#   description = "The User Name to use for authentication against the registry to pull the image."
+#   default     = "Welcome@1234"
+
+# }
+variable "java_embedded_server_enabled" {
+  type        = string
+  description = "value"
+  default     = ""
+
+}
+variable "tomcat_version" {
+  type        = string
+  description = "value"
+  default     = ""
+
+}
+
+
+variable "vnet_route_all_enabled" {
+  type        = bool
+  description = "enable all outbound"
+}
+variable "physical_path" {
+  type        = string
+  description = "value"
+  default     = "site\\wwwroot\\"
+
+}
+variable "virtual_path" {
+  type        = string
+  description = "value"
+  default     = "/"
+
+}
+variable "physical_path1" {
+  type        = string
+  description = "value"
+
+}
+variable "virtual_path1" {
+  type        = string
+  description = "value"
+
+}
+
+
+variable "docker_registry_url" {
+  type        = string
+  description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
+  default     = "https://mcr.microsoft.com"
+}
+variable "docker_registry_username" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "admin"
+
+}
+variable "docker_registry_password" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "Welcome@1234"
+
+}
+variable "python" {
+  type        = bool
+  description = "value"
+  default     = false
+
+}
+variable "https_only" {
+  type    = bool
+  default = true
+
+}
+
+variable "virtual_network_subnet_id" {
+  type        = string
+  description = "vnet link"
+}
+
+
+variable "ip_restrictions" {
+  type = map(object({
+    ip_address = string
+    priority   = number
+  }))
+  default = {
+    cf1  = { ip_address = "173.245.48.0/20", priority = 100 }
+    cf2  = { ip_address = "103.21.244.0/22", priority = 100 }
+    cf3  = { ip_address = "103.22.200.0/22", priority = 100 }
+    cf4  = { ip_address = "103.31.4.0/22", priority = 100 }
+    cf5  = { ip_address = "141.101.64.0/18", priority = 100 }
+    cf6  = { ip_address = "108.162.192.0/18", priority = 100 }
+    cf7  = { ip_address = "190.93.240.0/20", priority = 100 }
+    cf8  = { ip_address = "188.114.96.0/20", priority = 100 }
+    cf9  = { ip_address = "197.234.240.0/22", priority = 100 }
+    cf10 = { ip_address = "198.41.128.0/17", priority = 100 }
+    cf11 = { ip_address = "162.158.0.0/15", priority = 100 }
+    cf12 = { ip_address = "104.16.0.0/13", priority = 100 }
+    cf13 = { ip_address = "104.24.0.0/14", priority = 100 }
+    cf14 = { ip_address = "172.64.0.0/13", priority = 100 }
+    cf15 = { ip_address = "131.0.72.0/22", priority = 100 }
+  }
+}
+
+# private endpoint
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+  default     = ["sites"]
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
+}
+
+variable "use_32_bit_worker" {
+  type        = bool
+  description = "Specifies whether the Azure Windows Web App should run using a 32-bit worker process instead of the default 64-bit worker process."
+  default     = false
+}
